b65a8305516687e90d851bde00a563616e4cad90
MaximilianScheel <scheel.development@gmail.com>
2023-08-14 12:43:52 +0200

    first commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Bottombar from '@/components/shared/Bottombar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Topbar from '@/components/shared/Topbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,11 +21,22 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body>
-        <h1>Hallo</h1>
-      </body>
-    </html>
+     <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
